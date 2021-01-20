a6c8bc89a35f456d695ded5bfad151cbb5ab2e8c
sirm <sunyuping2018@163.com>
2021-01-20 14:52:25 +0800

    链表：删除链表中的节点

---
+++ 4-linked/src/easy/DeleteNode.java
@@ -0,0 +1,19 @@
+package easy;
+
+/**
+ * 删除链表中的节点
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为 要被删除的节点 。
+ *
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnarn7/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+
+
+public class DeleteNode {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
