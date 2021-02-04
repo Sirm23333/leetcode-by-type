1932d9d0c5208c37c6398d050aced3dc7f5bdb73
Sirm23333 <sunyuping2018@163.com>
2021-02-04 18:54:53 +0800

    数组：矩阵置零(中等)

--- 2-array/src/medium/SetZeroes.java
+++ 2-array/src/medium/SetZeroes.java
@@ -44,5 +44,59 @@ public class SetZeroes {
 
     }
 
+    /**
+     * 不用额外的两个数s组，而是用matrix[0][j]和matrix[i][0]记录
+     * 空间复杂度为O(1)
+     * @param matrix
+     */
+    public void setZeroes2(int[][] matrix) {
+        int m = matrix.length;
+        if(m < 1)
+            return;
+        int n = matrix[0].length;
+        boolean flag1 = false , flag2 = false;
+        for(int j = 0; j < n; j++){
+            if(matrix[0][j] == 0){
+                flag1 = true;
+                break;
+            }
+        }
+        for(int i = 0; i < m; i++){
+            if(matrix[i][0] == 0){
+                flag2 = true;
+                break;
+            }
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for(int j = 1; j < n; j++){
+            if(matrix[0][j] == 0){
+                for(int i = 0; i < m; i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < m; i++){
+            if(matrix[i][0] == 0){
+                for(int j = 0; j < n; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(flag1){
+            for(int j = 0; j < n; j++)
+                matrix[0][j] = 0;
+        }
+        if(flag2){
+            for(int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+        }
+    }
 
 }
