c463fca93968cc6bd56a81e1affd18f21eeaefbd
Sirm23333 <sunyuping2018@163.com>
2021-02-06 13:17:15 +0800

    数组：最长回文子串,暴力法(中等)

---
+++ 3-string/src/medium/LongestPalindrome.java
@@ -0,0 +1,67 @@
+package medium;
+
+/**
+ *最长回文子串
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ */
+public class LongestPalindrome {
+    /**
+     * 暴力法 O(n^3)
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        char[] chars = s.toCharArray();
+        int max = Integer.MIN_VALUE , start = 0, end = 0;
+        for(int i = 0; i < len; i++){
+            for(int j = i; j < len; j++){
+                if(validPalindrome(chars , i , j)){
+                    if(j - i + 1 > max){
+                        max = j - i + 1;
+                        start = i;
+                        end = j;
+                    }
+                }
+            }
+        }
+        return s.substring(start , end + 1);
+    }
+    private boolean validPalindrome(char[] chars , int start , int end){
+        while(start < end){
+            if(chars[start] != chars[end])
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    /**
+     * 中心扩散法
+     * @param s
+     * @return
+     */
+    public String longestPalindrome2(String s) {
+        int len = s.length();
+        char[] chars = s.toCharArray();
+        int max = Integer.MIN_VALUE,center = 0;
+        for(int i = 0; i < len - 1; i++){
+            int len1 = expandAroundCenter(chars , i , i) * 2 + 1; // 中心是一个数
+            int len2 = expandAroundCenter(chars , i , i+1) * 2 + 2; // 中心是一个位置
+            if(len1 > len2 && len1 > max){
+                max = len1;
+                center = i;
+            }else if(len2 >= len1 && len2 > max){
+                max = len2;
+                center = i;
+            }
+        }
+        return  s.substring(center - max / 2, max % 2 == 0 ? 1 : 0 + center + max / 2 + 1);
+    }
+    private int expandAroundCenter(char[] chars , int i, int j){
+        int len = 0;
+        while(i >= 0 && j < len && chars[i--] == chars[j++]) len++;
+        return len;
+    }
+}
