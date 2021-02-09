d9f7ddbc62909c6fd79db85d9fcc7f9d75243e26
Sirm23333 <sunyuping2018@163.com>
2021-02-09 17:27:56 +0800

    剑指offer：替换空格

---
+++ 11-offer/src/easy/ReplaceSpace.java
@@ -0,0 +1,18 @@
+package easy;
+
+/**
+ *剑指 Offer 05. 替换空格
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ */
+public class ReplaceSpace {
+    public String replaceSpace(String s) {
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == ' ')
+                sb.append("%20");
+            else
+                sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
