3fce8168d95dc386358fe44a52e6a451971626d3
sirm <sunyuping2018@163.com>
2021-02-21 13:10:56 +0800

    剑指offer：包含 min 函数的栈

---
+++ 11-offer/src/easy/MinStack.java
@@ -0,0 +1,34 @@
+package easy;
+
+import java.util.Stack;
+
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> min;
+
+    public MinStack() {
+        stack = new Stack<>();
+        min = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if(min.isEmpty())
+            min.push(x);
+        else
+            min.push(Math.min(x,min.peek()));
+    }
+
+    public void pop() {
+        stack.pop();
+        min.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int min() {
+        return min.peek();
+    }
+}
