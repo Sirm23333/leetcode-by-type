cd112fc7371b890dc5195d878067bede24a06077
sirm <sunyuping2018@163.com>
2021-02-24 13:07:45 +0800

    动态规划：跳跃游戏

--- 7-dynamic-programming/src/medium/UniquePaths.java
+++ 7-dynamic-programming/src/medium/UniquePaths.java
@@ -10,13 +10,41 @@ package medium;
  *
  */
 public class UniquePaths {
+    /**
+     * 递归 超时
+     * @param m
+     * @param n
+     * @return
+     */
     public int uniquePaths(int m, int n) {
         if(m == 0 || n == 0)
             return 0;
         if(m == 1)
-            return n - 1;
+            return 1;
         if(n == 1)
-            return m - 1;
+            return 1;
         return uniquePaths(m - 1,n) + uniquePaths(m,n-1);
+    }
+
+    /**
+     * dp
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths2(int m, int n) {
+        if(m == 0 || n == 0)
+            return 0;
+        int[][] arr = new int[m][n];
+        arr[0][0] = 1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || j == 0)
+                    arr[i][j] = 1;
+                else
+                    arr[i][j] = arr[i-1][j] + arr[i][j-1];
+            }
+        }
+        return arr[m-1][n-1];
     }
 }
