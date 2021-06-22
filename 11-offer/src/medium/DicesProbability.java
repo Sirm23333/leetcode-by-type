9355cec17c25e108649234926280eefe26feceb8
Sirm <sunyuping2018@163.com>
2021-06-22 17:38:16 +0800

    剑指 Offer 33. 二叉搜索树的后序遍历序列 剑指 Offer 60. n个骰子的点数

--- 11-offer/src/medium/DicesProbability.java
+++ 11-offer/src/medium/DicesProbability.java
@@ -16,11 +16,35 @@ package medium;
  * 输出: [0.16667,0.16667,0.16667,0.16667,0.16667,0.16667]
  */
 public class DicesProbability {
-    public double[] dicesProbability(int n) {
+    public static double[] dicesProbability(int n) {
         int min = n ;
         int max = n * 6;
         int total = (int) Math.pow(6,n);
         double[] rs = new double[max - min + 1];
+        int[][] dp = new int[n + 1][max + 1];
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        dp[1][3] = 1;
+        dp[1][4] = 1;
+        dp[1][5] = 1;
+        dp[1][6] = 1;
+        for(int i = 2; i < n + 1; i++){
+            for(int j = 1; j < max + 1; j++){
+                dp[i][j] = dp[i-1][j-1] +
+                        (j >= 2 ? dp[i-1][j-2] : 0) +
+                        (j >= 3 ? dp[i-1][j-3] : 0) +
+                        (j >= 4 ? dp[i-1][j-4] : 0) +
+                        (j >= 5 ? dp[i-1][j-5] : 0) +
+                        (j >= 6 ? dp[i-1][j-6] : 0);
+            }
+        }
+        for(int i = 0; i < max - min + 1; i++){
+            rs[i] = (double)dp[n][min + i] / total;
+        }
         return rs;
+    }
+
+    public static void main(String[] args) {
+        dicesProbability(2);
     }
 }
